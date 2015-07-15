9abac415451c7f7ee1544c076ff3be7c71059ae9
s.fundatureanu <sever.fundatureanu@sanoma.com>
2015-07-15 09:22:41 +0200

    Code cleanup

--- src/main/scala/com/adaptive/TrendingHashTags.scala
+++ src/main/scala/com/adaptive/TrendingHashTags.scala
@@ -6,12 +6,17 @@ import org.apache.spark.streaming.twitter._
 import org.apache.spark.SparkConf
 import com.twitter.finagle.redis.{TransactionalClient, Client}
 import com.twitter.finagle.redis.util.StringToChannelBuffer
-import com.twitter.finagle.redis.protocol.{Command, ZIncrBy}
+import com.twitter.finagle.redis.protocol.{Command, ZAdd, LTrim, ZMember, ZRemRangeByRank}
 import com.twitter.finagle.redis.protocol.Reply
 import com.twitter.finagle.redis.protocol.ErrorReply
 import com.twitter.util.Future
 import com.twitter.util.Await
 import twitter4j.Status
+import java.util.Properties
+import java.io.FileInputStream
+import org.apache.log4j.{Level, Logger}
+//import java.util.Properties
+//import java.io.FileInputStream
 
 /**
  * Calculates popular hashtags (topics) over sliding 10 and 60 second windows from a Twitter
@@ -25,8 +30,36 @@ object TrendingHashTags {
   
   object CONSTANTS {
     val WINDOW_SIZE_SECS = 120
-    val BATCH_DURATION = 2
+    val BATCH_DURATION : Int = 2
+    val TOP_TRENDING_TAGS : Int = 10
   }
+  
+  def main(args: Array[String]) {
+       
+    val filters = args
+        
+    val (stream, ssc) = createTwitterSparkStream(filters)
+        
+    val hashTags = stream.flatMap(status => extractHashTag(status))
+
+    // get the frequency of each hashTag in the specified window
+    val unsortedTagCountPairs = hashTags.map( tag => (tag, 1))
+            .reduceByKeyAndWindow((a, b) => a+b, (a, b) => a-b, Seconds(CONSTANTS.WINDOW_SIZE_SECS))          
+                      
+    // get top X trending hashtags from the window
+    val topCounts =  unsortedTagCountPairs.map{case (topic, count) => (count, topic)}
+                     .transform(_.sortByKey(false))
+
+    topCounts.foreachRDD(rdd => {
+      val topList = rdd.take(CONSTANTS.TOP_TRENDING_TAGS)
+      println("\nPopular topics in last 120 seconds (%s total):".format(rdd.count()))
+      insertMicroBatchIntoRedis(topList)
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+  
   
   def extractHashTag(status : Status) : Array[String] = {
     return status.getText.split(" ").filter(_.startsWith("#"))
@@ -34,13 +67,12 @@ object TrendingHashTags {
   
   def insertMicroBatchIntoRedis(topList : Array[(Int, String)]) = {
         val redisClient = TransactionalClient("10.51.78.48:6379")
-        redisClient.select(0)
+        redisClient.select(1)
         
-        var sequence: Seq[Command] = Seq()
-        topList.foreach { 
-          //case (count, tag) => println("%s (%s tweets)".format(tag, count))        
+        var sequence: Seq[Command] = Seq(ZRemRangeByRank(StringToChannelBuffer("top_tags"), 0, -1))
+        topList.foreach {         
           case (count, tag) => 
-                sequence ++= Seq(ZIncrBy(StringToChannelBuffer("global_trends"), count, StringToChannelBuffer(tag)))        
+                sequence ++= Seq(ZAdd(StringToChannelBuffer("top_tags"), Seq(ZMember(count, StringToChannelBuffer(tag)))))        
         }
         println("\nCommands total: %s:".format(sequence.size))
 
@@ -51,62 +83,24 @@ object TrendingHashTags {
         redisClient.quit();
   }
   
-  def main(args: Array[String]) {
-    if (args.length < 4) {
-      System.err.println("Usage: TrendingHashTags <consumer key> <consumer secret> " +
-        "<access token> <access token secret> [<filters>]")
-      System.exit(1)
-    }
-
-    StreamingExamples.setStreamingLogLevels()
-
-    val Array(consumerKey, consumerSecret, accessToken, accessTokenSecret) = args.take(4)
-    val filters = args.takeRight(args.length - 4)
-
+  def createTwitterSparkStream(filters: Array[String]) = {
     // Set the system properties so that Twitter4j library used by twitter stream
     // can use them to generat OAuth credentials
-    System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
-    System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
-    System.setProperty("twitter4j.oauth.accessToken", accessToken)
-    System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
-
-    val sparkConf = new SparkConf().setMaster("local[10]").setAppName("TrendingHashTags")
+    val prop = new Properties()
+    prop.load(new FileInputStream("config.properties"))
     
-    val ssc = new StreamingContext(sparkConf, Seconds(CONSTANTS.BATCH_DURATION))
+    System.setProperty("twitter4j.oauth.consumerKey", prop.getProperty("consumerKey"))
+    System.setProperty("twitter4j.oauth.consumerSecret", prop.getProperty("consumerSecret"))
+    System.setProperty("twitter4j.oauth.accessToken", prop.getProperty("accessToken"))
+    System.setProperty("twitter4j.oauth.accessTokenSecret", prop.getProperty("accessTokenSecret"))
+    
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("TrendingHashTags")  
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    ssc.checkpoint("/tmp")
+    
     val stream = TwitterUtils.createStream(ssc, None, filters)
-    
-    
-
-    /*val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
-                     .map{case (topic, count) => (count, topic)}
-                     .transform(_.sortByKey(false))
-    // Print popular hashtags
-    topCounts60.foreachRDD(rdd => {
-      val topList = rdd.take(10)
-      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
-      topList.foreach{
-        case (count, tag) => println("%s (%s tweets)".format(tag, count))
-        
-      }
-    })*/
-    
-    val hashTags = stream.flatMap(status => extractHashTag(status))
-
-    val unsortedTagCountPairs = hashTags.map( tag => (tag, 1))
-            .reduceByKeyAndWindow((a, b) => a+b, (a, b) => a-b, Seconds(CONSTANTS.WINDOW_SIZE_SECS))
-            //.countByValueAndWindow(Seconds(CONSTANTS.WINDOW_SIZE_SECS), Seconds(CONSTANTS.BATCH_DURATION))          
-                      
-    val topCounts2sec =  unsortedTagCountPairs.map{case (topic, count) => (count, topic)}
-                     .transform(_.sortByKey(false))
-
-    topCounts2sec.foreachRDD(rdd => {
-      val topList = rdd.take(10)
-      println("\nPopular topics in last 2 seconds (%s total):".format(rdd.count()))
-      insertMicroBatchIntoRedis(topList)
-    })
-
-    ssc.start()
-    ssc.awaitTermination()
+    (stream, ssc)
   }
+    
 }
-// scalastyle:on println
+
